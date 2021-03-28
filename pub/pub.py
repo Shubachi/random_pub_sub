eaba0a0b2e944a6bc5d30059f0a561010cb34b3c
Samuel Cheung <shuabchi@gmail.com>
2021-03-27 21:52:18 -0400

    change some pub configs to env var

--- pub/pub.py
+++ pub/pub.py
@@ -1,4 +1,6 @@
 from datetime import datetime, timedelta
+
+import os
 import time
 import random
 import redis
@@ -6,28 +8,28 @@ import sys
 
 # python does not define a max integer value, but we'll define it as max of uint here
 UINT_MAX = 4294967295
-PUBLISH_RATE_PER_SECOND = 20
-SUBSCRIPTION_NAME = 'pub_sub_test'
 
-g_connection = redis.Redis(host='localhost', port=6379, db=0)
+def publish_random_int(publish_period, subscription_name):
+    redis_connection = redis.Redis(host='localhost', port=6379, db=0)
 
-def publish_random_int():
-    global g_connection
-    current_value = random.randint(0, UINT_MAX)
-    # print("publish {}".format(current_value))
-    g_connection.publish(SUBSCRIPTION_NAME, current_value)
-
-def schedule_publisher():
-    publish_period = 1 / PUBLISH_RATE_PER_SECOND
     while True:
         next_time = datetime.now() + timedelta(seconds=publish_period)
 
-        publish_random_int()
+        current_value = random.randint(0, UINT_MAX)
+        # print("publish {}".format(current_value))
+        redis_connection.publish(subscription_name, current_value)
 
         # To account for time it took to send the message, we can't just do a raw sleep
         # Otherwise we'll always be a bit slower than we wanted to be
         time.sleep((next_time - datetime.now()).total_seconds())
 
 if __name__ == "__main__":
-    schedule_publisher()
+    subscription_name = os.getenv("SUBCRIPTION_NAME", 'pub_sub_test')
+    publish_rate = os.getenv("PUB_RATE_PER_SECOND", 20)
+    publish_period =  1 / publish_rate
+
+    publish_random_int(
+        publish_period=publish_period, 
+        subscription_name=subscription_name
+    )
     
