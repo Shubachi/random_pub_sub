d6f064fe2161e10374bf0ba18e169253016f7e9e
Samuel Cheung <shuabchi@gmail.com>
2021-03-27 21:42:18 -0400

    first working implementation of random pub

--- pub/pub.py
+++ pub/pub.py
@@ -1,22 +1,33 @@
-from apscheduler.schedulers.blocking import BlockingScheduler 
-
+from datetime import datetime, timedelta
+import time
+import random
 import redis
+import sys
 
-alice_r = redis.Redis(host='localhost', port=6379, db=0)
+# python does not define a max integer value, but we'll define it as max of uint here
+UINT_MAX = 4294967295
+PUBLISH_RATE_PER_SECOND = 20
+SUBSCRIPTION_NAME = 'pub_sub_test'
 
-def some_job():
-    print("Every 2 seconds")
+g_connection = redis.Redis(host='localhost', port=6379, db=0)
 
+def publish_random_int():
+    global g_connection
+    current_value = random.randint(0, UINT_MAX)
+    # print("publish {}".format(current_value))
+    g_connection.publish(SUBSCRIPTION_NAME, current_value)
 
-def publisher():
-    scheduler = BlockingScheduler()
-    scheduler.add_job(some_job, 'interval', seconds=2)
-    scheduler.start()
+def schedule_publisher():
+    publish_period = 1 / PUBLISH_RATE_PER_SECOND
+    while True:
+        next_time = datetime.now() + timedelta(seconds=publish_period)
 
-def redis_test():
-    alice_r.publish('classical_music', 'Alice Music')
+        publish_random_int()
+
+        # To account for time it took to send the message, we can't just do a raw sleep
+        # Otherwise we'll always be a bit slower than we wanted to be
+        time.sleep((next_time - datetime.now()).total_seconds())
 
 if __name__ == "__main__":
-    #publisher()
-    redis_test()
+    schedule_publisher()
     
--- sub_sum/sub_sum.py
+++ sub_sum/sub_sum.py
@@ -1,10 +1,11 @@
 import redis
 
-if __name__ == "__main__":
+SUBSCRIPTION_NAME = 'pub_sub_test'
 
+if __name__ == "__main__":
     r = redis.Redis(host='localhost', port=6379, db=0)
     p = r.pubsub()
-    p.psubscribe('classical_music')
+    p.psubscribe(SUBSCRIPTION_NAME)
     
     for new_message in p.listen():
         print(new_message)
